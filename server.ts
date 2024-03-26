c19eaedaa12f2c792b18b35c2952b7960697fedb
Ian-Bright <ianbrighton1@gmail.com>
2024-03-26 09:20:19 -0600

    feat: Timeout trigger websocket event fixed

--- server.ts
+++ server.ts
@@ -9,13 +9,13 @@ import dotenv from "dotenv";
 import { game } from "./controllers/game";
 import { initContract } from "./utils/contract";
 import {
-  finalizeTurn,
-  joinGame,
-  openChannel,
-  signOpponentTurn,
-  startGame,
-  timeoutTriggered,
-  turn,
+    finalizeTurn,
+    joinGame,
+    openChannel,
+    signOpponentTurn,
+    startGame,
+    timeoutTriggered,
+    turn,
 } from "./controllers/socketEvents";
 dotenv.config();
 
@@ -25,37 +25,39 @@ const app = express();
 const port = 8000;
 const server = createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-  },
+    cors: {
+        origin: "http://localhost:5173",
+    },
 });
 
 app.use(bodyParser.json());
 app.use(cors());
 
 server.listen(port, async () => {
-  console.log("Connecting to DB...");
-  const client = new DBClient(MONGO_URL!, MONGO_DB_NAME!);
-  await client.init();
+    console.log("Connecting to DB...");
+    const client = new DBClient(MONGO_URL!, MONGO_DB_NAME!);
+    await client.init();
 
-  // check for existing contract or deploy new one
-  await initContract(client);
+    // check for existing contract or deploy new one
+    await initContract(client);
 
-  // Register HTTP routes
-  app.use("/game", game(client));
-  app.use("/user", user(client));
+    // Register HTTP routes
+    app.use("/game", game(client));
+    app.use("/user", user(client));
 
-  // Setup up socket.io event listeners
-  io.on("connection", (socket: Socket) => {
-    // socket.on('game:answerTimeout')
-    socket.on("game:join", joinGame(socket, client));
-    socket.on("game:openChannel", openChannel(socket, client));
-    socket.on("game:finalizeTurn", finalizeTurn(socket, client));
-    socket.on("game:signOpponentTurn", signOpponentTurn(socket, client));
-    socket.on("game:start", startGame(socket, client));
-    socket.on("game:timeoutTriggered", timeoutTriggered(socket, client));
-    socket.on("game:turn", turn(socket, client));
-  });
+    // Setup up socket.io event listeners
+    io.on("connection", (socket: Socket) => {
+        // socket.on('game:timeoutAnswered');
+        socket.on("game:join", joinGame(socket, client));
+        socket.on("game:openChannel", openChannel(socket, client));
+        socket.on("game:finalizeTurn", finalizeTurn(socket, client));
+        socket.on("game:signOpponentTurn", signOpponentTurn(socket, client));
+        socket.on("game:start", startGame(socket, client));
+        // socket.on('game:timeoutAnswered');
+        // socket.on('game:timeoutDisputed');
+        socket.on("game:timeoutTriggered", timeoutTriggered(socket, client));
+        socket.on("game:turn", turn(socket, client));
+    });
 
-  console.log(`Tic Tac Aztec Server listening at http://localhost:${port}`);
+    console.log(`Tic Tac Aztec Server listening at http://localhost:${port}`);
 });
