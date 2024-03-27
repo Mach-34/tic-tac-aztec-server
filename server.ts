af7dcea7b6c1ccc4d31612d8dea705f6b5764ea2
Jack Gilcrest <me@jp4g.llc>
2024-03-26 18:34:24 -0600

    added nudge function and updated imports to point at state channel repo

--- server.ts
+++ server.ts
@@ -3,11 +3,13 @@ import cors from "cors";
 import { Server, Socket } from "socket.io";
 import { createServer } from "node:http";
 import bodyParser from "body-parser";
+import { createAccount } from "@aztec/accounts/testing";
+import { createPXEClient } from "@aztec/aztec.js";
 import { user } from "./controllers/user";
 import DBClient from "./mongo";
 import dotenv from "dotenv";
 import { game } from "./controllers/game";
-import { initContract } from "./utils/contract";
+import { initContract, nudge } from "./utils/contract";
 import {
   finalizeTurn,
   joinGame,
@@ -17,9 +19,10 @@ import {
   timeoutTriggered,
   turn,
 } from "./controllers/socketEvents";
+
 dotenv.config();
 
-const { MONGO_DB_NAME, MONGO_URL } = process.env;
+const { MONGO_DB_NAME, MONGO_URL, PXE_URL } = process.env;
 
 const app = express();
 const port = 8000;
@@ -38,8 +41,12 @@ server.listen(port, async () => {
   const client = new DBClient(MONGO_URL!, MONGO_DB_NAME!);
   await client.init();
 
+  // init connection to pxe
+  const pxe = createPXEClient(PXE_URL!);
+  const deployer = await createAccount(pxe);
+
   // check for existing contract or deploy new one
-  await initContract(client);
+  const contractAddress = await initContract(client, deployer);
 
   // Register HTTP routes
   app.use("/game", game(client));
@@ -58,4 +65,9 @@ server.listen(port, async () => {
   });
 
   console.log(`Tic Tac Aztec Server listening at http://localhost:${port}`);
+
+  // set interval for calling nudge every 30 seconds
+  setInterval(() => {
+    nudge(contractAddress, deployer);
+  }, 30000);
 });
